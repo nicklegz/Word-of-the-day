19f6d8b4c39d294d9baa8713003e221c03cdad7c
Nick LeGuerrier <nicklegz@Nicks-MacBook-Air.local>
2021-11-08 22:27:24 -0500

    Added production settings and express

--- web_ui/word-of-the-day/src/environments/environment.prod.ts
+++ web_ui/word-of-the-day/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiURL: "https://localhost:5001/api",
+  apiURL: "https://api-worddujour.herokuapp.com/api",
   domain: "nicklegz.us.auth0.com",
 };
