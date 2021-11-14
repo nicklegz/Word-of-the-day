26c5a433718703d97a583a0a16ed29b7b434670e
Nick LeGuerrier <nicklegz@Nicks-MacBook-Air.local>
2021-11-14 15:49:32 -0500

    Added Auth guard

--- web_ui/word-of-the-day/src/app/app-routing.module.ts
+++ web_ui/word-of-the-day/src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth.guard';
 import { ErrorComponent } from './components/error/error.component';
 import { WordComponent } from './components/word/word.component';
 
 const routes: Routes = [
-  {path: 'home', component: WordComponent},
+  {path: 'home', component: WordComponent, canActivate:[AuthGuard]},
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'error', component: ErrorComponent}
-
 ];
 
 @NgModule({
--- web_ui/word-of-the-day/src/app/app.component.ts
+++ web_ui/word-of-the-day/src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
 import { UserInfo } from './interfaces/userAuth.interface';
-import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -12,14 +10,6 @@ export class AppComponent implements OnInit{
   title = 'word-of-the-day';
   userInfo!: UserInfo;
 
-  constructor(private auth: AuthService){}
-
-  ngOnInit(): void{
-    this.auth.getUserInfo().subscribe(data =>{
-      if(data.isAuthenticated == false){
-        this.auth.login();
-      }
-
-    })
+  ngOnInit(): void{    
   }
 }
