9e42a0357044187ac9d27f0898a54a18fa69da47
Nick LeGuerrier <nicklegz@Nicks-MacBook-Air.local>
2021-11-14 22:22:16 -0500

    Modified routing

--- web_ui/word-of-the-day/src/app/app-routing.module.ts
+++ web_ui/word-of-the-day/src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './auth.guard';
-import { ErrorComponent } from './components/error/error.component';
 import { WordComponent } from './components/word/word.component';
 
 const routes: Routes = [
-  {path: '', component: WordComponent, canActivate:[AuthGuard]},
-  {path: '**', redirectTo: ''}
+  {path: 'home', component: WordComponent, canActivate:[AuthGuard]},
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: '**', redirectTo: '/home'}
 ]; 
 
 @NgModule({
