53918f47ea692e920edb16b0a80f058e96a51f76
Nick LeGuerrier <nicklegz@Nicks-MacBook-Air.local>
2021-11-15 23:45:52 -0500

    Modified paths in enviro files

--- web_ui/word-of-the-day/src/environments/environment.prod.ts
+++ web_ui/word-of-the-day/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  apiURL: "https://api.worddujour.ca/api",
+  apiURL: "https://api-worddujour.herokuapp.com/api",
   domain: "nicklegz.us.auth0.com",
+  clientId: "TmknT9w8R5SmNfxFymDW1TDAPiPcKPVY"
 };
