124e7b3d5c1fc41441c1f0c40938d889e98a119f
Nick LeGuerrier <nicklegz@Nicks-MacBook-Air.local>
2021-11-17 18:02:39 -0500

    Added redirect to https in server.js file

--- web_ui/word-of-the-day/server.js
+++ web_ui/word-of-the-day/server.js
@@ -6,6 +6,13 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/word-of-the-day'));
 
+app.use((req, res, next) => {
+    if (req.header('x-forwarded-proto') !== 'https')
+      res.redirect(`https://${req.header('host')}${req.url}`)
+    else
+      next()
+})
+
 app.get('/', function(req,res) {
     
 res.sendFile(path.join(__dirname+'/dist/word-of-the-day/index.html'));
