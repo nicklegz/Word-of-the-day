31364cee0baa937c3de2acff2544a71645f8af8f
Nick LeGuerrier <nicklegz@Nicks-MacBook-Air.local>
2021-11-16 19:05:05 -0500

    Modified auth interceptor

--- web_ui/word-of-the-day/src/app/services/auth.interceptor.ts
+++ web_ui/word-of-the-day/src/app/services/auth.interceptor.ts
@@ -12,9 +12,9 @@ export class AuthInterceptor implements HttpInterceptor {
   constructor(private router: Router) {}
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    request = request.clone({
-      withCredentials: true
-  });
+  //   request = request.clone({
+  //     withCredentials: true
+  // });
     return next.handle(request).pipe(
       retryWhen(errors => errors.pipe(
         tap(error =>{
